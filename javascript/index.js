ec7192dd4dfcce1cfe9ed3093659ad56ffbf22aa
Sergi Vaqué Galdón <sergivaque@gmail.com>
2023-03-11 15:57:35 +0100

    Contact us done

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,17 @@
   const nav = document.querySelector('.nav');
+  const contact_li = document.querySelector('#contact-li');
+
   const burguerOn = document.querySelector('.burguer-icon-on');
   const burguerOff = document.querySelector('.burguer-icon-off');
 
   burguerOn.addEventListener("click", () => {
     nav.classList.add("visible");
+    contact_li.style.display = 'block';
   })
 
   burguerOff.addEventListener("click", () => {
-    nav.classList.remove("visible");
+      nav.classList.remove("visible"); 
+      contact_li.style.display = 'none';         
   })
 
 
